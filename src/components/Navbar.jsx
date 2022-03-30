86d8faba2968aca3e78ec91b473490bdcddeda89
elena souto <elenasouto@gmail.com>
2022-03-30 21:08:39 +0100

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#64676b] text-gray-200'>
+    </div>
+  )
+}
+
+export default Navbar
