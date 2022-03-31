435525b7ccffd91e04e8bd540a9eca4dfe6b1469
elena souto <elenasouto@gmail.com>
2022-03-31 21:43:59 +0100

    projects page added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,15 +4,16 @@ import GitHub from '../assets/github.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
 import Tailwind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png';
+
+
 
 
 const Skills = () => {
   return (
-    <div name='Skills' className='bg-[#64676b] text-black '>
+    <div name='Skills' className='w- full h-screen bg-[#64676b] text-gray-200 '>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
            <div>
-               <p className='text-2xl font-normal inline border-2 px-6 py-3 my-2 border-white'>Tech Stack</p>
+           <p className='text-3xl font-medium inline border-b-4 border-pink-500 '>Skills</p>
                <p className='py-4'>Tech I have used:</p>
            </div> 
 {/* containers for all icons */}
@@ -37,10 +38,8 @@ const Skills = () => {
                   <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
                   <p className='my-4'>TAILWIND</p>
               </div>
-              <div className='shadow-md shadow-[#5a5c60] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
-                  <p className='my-4'>MONGO DB</p>
-              </div>
+             
+              
            </div>
         </div>
     </div>
@@ -49,4 +48,3 @@ const Skills = () => {
 
 export default Skills
 
-{/* text-black group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-500 hover:border-pink-500'> */}
---
+++ src/components/Work.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#64676b]'>
+    <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+
+    {/* This stores all the work */}
+    <div className='pb-8'>
+    <p className='text-3xl font-bold inline border-b-4 text-gray-200 border-pink-500'>
+    Projects
+
+    </p>
+        <p>Please take a look at some of my work</p>  
+    </div>
+        
+        <div>
+        <div>
+            <div>
+                <span>
+
+                </span>
+                <div>
+                    <a href="/">
+                   <button></button>
+                   </a>
+
+                    <a href="/">
+                   <button></button>
+                   </a>
+                </div>
+            </div>
+        </div>
+
+        </div>    
+        
+    </div>
+    </div>
+  )
+}
+
+export default Work
